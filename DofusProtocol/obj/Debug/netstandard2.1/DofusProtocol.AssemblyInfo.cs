285fa30eb36eb8c8b9cd41393f0bfa037c354264
ImNotARobot742 <alemairedem@ensc.fr>
2023-05-18 16:44:47 +0200

    15

--- DofusProtocol/obj/Debug/netstandard2.1/DofusProtocol.AssemblyInfo.cs
+++ DofusProtocol/obj/Debug/netstandard2.1/DofusProtocol.AssemblyInfo.cs
@@ -14,11 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImNotARobot742")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Dofus Protocol extracted using the AnkamaProtocol-Builder\r\n")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.67.10.14")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.67.10.14")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.67.10.15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.67.10.15")]
 [assembly: System.Reflection.AssemblyProductAttribute("DofusProtocol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DofusProtocol")]
-[assembly: System.Reflection.AssemblyVersionAttribute("2.67.10.14")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.67.10.15")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
